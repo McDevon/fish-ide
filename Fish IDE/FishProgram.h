7b7ae75763b3a44bccc7d17bdf3237b04fc5c48a
McDevon <jussi.enroos@saunalahti.fi>
2015-03-09 22:01:49 +0200

    File content reading

--- Fish IDE/FishProgram.h
+++ Fish IDE/FishProgram.h
@@ -19,4 +19,7 @@
 @property int executeAreaWidth;
 @property int executeAreaHeight;
 
+
++ (instancetype) createProgramFromFileContents:(NSString*) fishFileContents;
+
 @end
--- Fish IDE/FishProgram.m
+++ Fish IDE/FishProgram.m
@@ -10,4 +10,60 @@
 
 @implementation FishProgram
 
+- (instancetype)init
+{
+    if (self = [super init]) {
+        _originX = 0;
+        _originY = 0;
+
+        _lines = [[NSMutableArray alloc] init];
+    }
+    
+    return self;
+}
+
++ (instancetype)createProgramFromFileContents:(NSString *)fishFileContents
+{
+    FishProgram *prog = [[FishProgram alloc] init];
+    
+    // Go through the file and make each line a string for the program
+    const char *str = [fishFileContents cStringUsingEncoding:NSASCIIStringEncoding];
+    UInt32 i = 0;
+    size_t longest = 0;
+    int line = 0;
+    
+    while (str[i] != '\0') {
+        
+        if (str[i] == '\n') {
+            // Create a line and add to array
+            size_t textSize = i - line + 1;
+            
+            char *text = malloc(textSize);
+            strncpy(text, &str[line], textSize - 1);
+            text[textSize - 1] = '\0';
+            
+            NSString *string = [NSString stringWithUTF8String:text];
+            [prog.lines addObject:string];
+            
+            // Log the lines for now
+            NSLog(@"%@", string);
+            
+            free (text);
+            
+            // Set beginning of next line
+            line = i+1;
+            
+            // Check for longest lines
+            if (textSize-1 > longest) {
+                longest = textSize-1;
+            }
+        }
+    }
+    
+    prog.executeAreaWidth = (int)longest;
+    prog.executeAreaHeight = (int)[prog.lines count];
+    
+    return nil;
+}
+
 @end
